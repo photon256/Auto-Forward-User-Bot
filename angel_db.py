8114472c785ad3a4efb5dd183966a3507fbedc91
photon256 <144157217+photon256@users.noreply.github.com>
2025-05-02 10:46:38 +0530

    Update angel_db.py

--- angel_db.py
+++ angel_db.py
@@ -5,21 +5,24 @@ from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 
 load_dotenv()
+# ===== WOODcraft ==== SudoR2spr ==== #
 
 MONGO_URI = os.getenv("MONGO_URI")
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client["forwardBot"]
 
 # Collections
+# ===== WOODcraft ==== SudoR2spr ==== 
 collection = db["forwarded_files"]
 settings_col = db["settings"]
 admin_col = db["admins"]
 extra_targets_col = db["extra_targets"]
+# ===== WOODcraft ==== SudoR2spr ==== 
 
-# Ensure unique constraint
+# Index setup
 collection.create_index([("message_id", 1), ("target_id", 1)], unique=True)
 
-# Forwarding status
+# Utility functions
 async def is_forwarded_for_target(msg_id, target_id):
     return collection.find_one({"message_id": msg_id, "target_id": target_id}) is not None
 
@@ -28,4 +31,4 @@ async def mark_as_forwarded_for_target(msg_id, target_id):
         collection.insert_one({"message_id": msg_id, "target_id": target_id})
     except DuplicateKeyError:
         pass
-        
+    
